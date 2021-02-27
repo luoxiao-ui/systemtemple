888057f52a29b78632c91c331294e277f9f97590
zs <121120885@qq.com>
2021-02-27 09:59:58 +0800

    初始

---
+++ src/views/Baseset/goodsdept/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+      <h1>商品部门</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Goodsdept',
+  data() {
+    return {}
+},
+  watch: {},
+  computed: {},
+  created() {
+    console.log(`商品部门created`)
+  },
+  mounted() {
+  },
+  methods: {},
+}
+</script> 
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Baseset/goodsfile/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+      <h1>商品档案页</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Goodsfile',
+  data() {
+    return {}
+},
+  watch: {},
+  computed: {},
+  created() {
+    console.log(`商品档案created`)
+  },
+  mounted() {},
+  methods: {},
+}
+</script> 
+
+<style lang="scss" scoped>
+</style>
