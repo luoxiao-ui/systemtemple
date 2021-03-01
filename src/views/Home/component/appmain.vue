afb0ade1f72aa7e26d1131d4049da9a3bf1737cc
zs <121120885@qq.com>
2021-03-01 16:10:32 +0800

    测试

--- src/views/Home/component/appmain.vue
+++ src/views/Home/component/appmain.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="container">
     <transition name="fade-transform" mode="out-in">
+      <keep-alive :include="tablist">
       <router-view></router-view>
+      </keep-alive>
     </transition>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   data() {
     return {}
 },
   watch: {},
-  computed: {},
+  computed: {
+    ...mapGetters(['tabs']),
+    tablist() {
+        let a = []
+        this.tabs.forEach(item=> {
+          a.push(item.name)
+        })
+        return a.toString()
+    }
+  },
   created() {},
   mounted() {},
   methods: {},
